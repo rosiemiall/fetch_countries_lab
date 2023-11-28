947f9c239b7bde9643d149748f791eabb7312c1a
Rosie Miall <rosiemiall38@gmail.com>
2023-11-28 20:58:51 +0000

    mvp almost finished

---
+++ index.js
@@ -0,0 +1,58 @@
+
+const nameSection = document.querySelector("#nameSection")
+const populationSection = document.querySelector("#population")
+const capitalSection = document.querySelector("#capital")
+const languagesSection = document.querySelector("#languages")
+const getCountryByName = async (countryName) =>{
+    const response = await fetch("https://restcountries.com/v3.1/name/"+countryName);
+    const data = await response.json();
+    console.log(data[0]);
+
+    const population = data[0].population;
+    const popOutput = document.createElement("p");
+    popOutput.innerText = population;
+    populationSection.appendChild(popOutput);
+
+    const name = data[0].name.common;
+    const nameOutput = document.createElement("p")
+    nameOutput.innerText = name
+    nameSection.appendChild(nameOutput)
+
+    const languages = data[0].languages;
+    const langOutput = document.createElement("p")
+    const array = []
+    // for (i = 0; i < languages.length; i++){
+    //     array.push(languages[i])
+    // }
+    // Object.keys(languages).forEach((prop) => array.push(languages.prop))
+    // langOutput.innerText = array
+    langOutput.innerText =JSON.stringify(languages)
+    languagesSection.appendChild(langOutput)
+
+    const capital = data[0].captial;
+    const capitalOutput = document.createElement("p");
+    capitalOutput.innerText = JSON.stringify(capital);
+    capitalSection.appendChild(capitalOutput);
+
+
+    return data[0]
+}
+
+getCountryByName("peru");
+
+
+// const printDetails = (countryName) => {
+//     const country = getCountryByName(countryName);
+//     console.log(country);
+//     // const name = country.name.common;
+//     // const nameOutput = document.createElement("p")
+//     // nameOutput.innerText = name
+//     // section.appendChild(nameOutput)
+//     // console.log(name);
+//     // const population = country.population;
+//     // const popOutput = document.createElement("p");
+//     // popOutput.innerText = population;
+//     // section.appendChild(popOutput);
+// }
+
+// printDetails("peru")
